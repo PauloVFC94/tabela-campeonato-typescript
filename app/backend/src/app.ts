e3023a7b29f69202e6289d4ab6e02403c2a205fe
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-09-29 13:21:18 -0300

    Feat: Criação de Migration e Model da tabela user

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -10,6 +10,7 @@ class App {
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.get('/login',(req, res) => res.json({}))
   }
 
   private config():void {
