c7895f7951c7d20cf9b9af95a84281cec5b7fb7d
Paulo Ferreira <paulovictorfcosta@gmail.com>
2022-10-11 17:40:48 -0300

    Feat: adiciona migration da tabela match

---
+++ app/backend/src/database/migrations/20221011203633-create-matches.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('matches', {
+      id: { 
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      homeTeam: {
+        type: Sequelize.INTEGER,
+        field: 'home_team',
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id',
+        }
+      },
+      awayTeam: {
+        type: Sequelize.INTEGER,
+        field: 'away_team',
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id',
+        }
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        field: 'home_team_goals',
+        allowNull: false,
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        field: 'away_team_goals',
+        allowNull: false,
+      },
+      inProgress: {
+        type: Sequelize.INTEGER,
+        field: 'in_progress',
+        allowNull: false,
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('matches');
+  }
+};
